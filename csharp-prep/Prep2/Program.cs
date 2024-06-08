2c2489ce3fcb709a5ec573aaea1d1c5a0f5ee684
f-a-bejarano2023 <bej23002@byui.edu>
2024-06-08 03:23:07 -0300

    Updated the code to get the score from the user and print the grade letter according to it.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Get the score percentage from the user
+        Console.Write("PLease, enter your grade percentage ");
+        int gradePercent = int.Parse(Console.ReadLine());
+        
+        //Determine the grade letter depending on the score percentage
+        if (gradePercent  >  90)
+        {
+            Console.WriteLine("You received an A grade");
+        }
+        else if (gradePercent  >=  80)
+        {
+            Console.WriteLine("You received a B grade");
+        }
+        else if (gradePercent  >=  70)
+        {
+            Console.WriteLine("You received a C grade");
+        }
+        else if (gradePercent  >=  60)
+        {
+            Console.WriteLine("You received a D grade");
+        }
+        else if (gradePercent  <  60)
+        {
+            Console.WriteLine("You received an F grade");
+        }
+
+        if (gradePercent >= 70)
+        {
+            System.Console.WriteLine("You passed this course. Congratulations!");
+        }
+        else
+        {
+            System.Console.WriteLine("You barely made it. you can try again next block!");
+        }
     }
 }
