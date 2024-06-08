748c0c61fa7ffcad6668146ad4c5d680e1e543e2
f-a-bejarano2023 <bej23002@byui.edu>
2024-06-08 02:16:24 -0300

    Previous version was missing  the end period and came up with unidentified error, now corrected.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,10 +6,10 @@ class Program
     {
         Console.WriteLine("What is your first name?");
         string firstName = Console.ReadLine();
+
         Console.WriteLine("What is your last name?");
         string lastName = Console.ReadLine();
 
-        Console.Write("Your name is " + lastName + ", "+ firstName + " " + lastName.);
-        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
