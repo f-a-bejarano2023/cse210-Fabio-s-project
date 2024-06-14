fad9994ff972d1630bc3d54fa2cd168a25557a64
f-a-bejarano2023 <bej23002@byui.edu>
2024-06-14 03:26:28 -0300

    Added the code for the core requirements

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userOption;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        do
+        {
+            Console.Write("Please, enter a number: ");
+            userOption = Convert.ToInt32(Console.ReadLine());
+            
+            if (userOption == 0)
+                break;
+            else
+                numbers.Add(userOption);
+        
+        } while (true);
+
+        Console.WriteLine("Numbers entered: ");
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        // Calculate the sum of the numbers
+        int total = 0;
+        foreach (int num in numbers)
+        {
+            total += num;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        // Calculate the average of the numbers
+        double average = (double)total / numbers.Count;
+        Console.WriteLine($"The average is: {average:F2}");
+
+        // Find the largest number in the list
+        int largest = numbers.Count > 0 ? numbers.Max() : 0;
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
