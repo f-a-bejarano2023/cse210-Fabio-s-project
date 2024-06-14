b9758792370a843e88eea37b87e1e1cdd53075c7
f-a-bejarano2023 <bej23002@byui.edu>
2024-06-14 01:29:51 -0300

    added code for the Guess my number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+        
+        while (playAgain)
+        {
+            Random randomGenerator = new Random();
+            int secretNumber = randomGenerator.Next(1, 101);
+            int guessNumber = 0;
+
+            Console.WriteLine("Guess the secret number between 1 and 100:");
+
+            while (guessNumber != secretNumber)
+            {
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+
+                if (guessNumber < secretNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > secretNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+
+            Console.WriteLine("Now, You´ve done it!!!");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower() == "yes";
+        }
+
+        Console.WriteLine("Thank you for playing!!!");
     }
 }
