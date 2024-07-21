01ec6a0931a92a26df2e2d4a7787f3b9449ff861
f-a-bejarano2023 <bej23002@byui.edu>
2024-07-20 22:32:13 -0300

    Updated the code for the Learning05 project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 5, 3),
+            new Circle("Green", 6)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.Color}, Area: {shape.GetArea():F2}");
+        }
     }
 }
